be323ffb5dcebfcb9192b51e95b19489f387f49d
lobana alarabi <loubanaeng@gmail.com>
2020-02-03 23:14:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+<<<<<<< HEAD
+=======
+//first
+ 
+//     function submitData(name,email){
+// return fetch("http://localhost:3000/users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     name: name,
+//     email: email
+//   })
+// });
+      
+//     }
+
+
+
+
+>>>>>>> 1fc7e3f32c7464cb33f012bc19442e3d7328e29b
 function submitData(name,email){
 return fetch("http://localhost:3000/users", {
   method: "POST",
@@ -17,16 +40,21 @@ return fetch("http://localhost:3000/users", {
     for (x in object) {
   document.body.innerHTML+= object[x] + "<br>";
 }
+<<<<<<< HEAD
 
     //console.log(object);
   }).catch(function(error) {
     //alert("Bad things! Ragnarők!");
     document.body.innerHTML=error.message;
+=======
+    console.log(object);
+>>>>>>> 1fc7e3f32c7464cb33f012bc19442e3d7328e29b
   });
       
     }
     
     
+<<<<<<< HEAD
 //first 
 let name = "Steve"
 let email = "steve@steve.com"
@@ -40,4 +68,14 @@ submitData(name,email);
 //third
 name = "Jim"
 email = "jim@jim.com"
+=======
+    //first 
+    let name = "Steve"
+let email = "steve@steve.com"
+submitData(name,email);
+    
+    name = "Sam"
+    email = "sam@sam.com" 
+    
+>>>>>>> 1fc7e3f32c7464cb33f012bc19442e3d7328e29b
 submitData(name,email);
